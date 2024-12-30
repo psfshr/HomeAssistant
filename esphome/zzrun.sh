c28bbe6d2ce438ef453f90d6aa6fb3bbef88da8b
Paul Fisher <psfshr@me.com>
2024-12-30 10:34:13 +0000

    Tweaked ESPHome runner script

--- esphome/zzrun.sh
+++ esphome/zzrun.sh
@@ -2,13 +2,13 @@
 
 usage()
 {
-    echo "usage: zzrun.sh [-f file] | [-h]"
+    echo "usage: zzrun.sh [-o] [-f file]| [-h]"
 }
 
 runesphome ()
-{
+{ 
     if [ -f "$filename" ]; then
-        if [[ $filename =~ "-base" ]]; then
+        if [[ $filename =~ "-base" ]] && $override=0; then
             echo "Trying to install a base file, please run manually!!"
         else
             echo "Running esphome for file => $filename"
@@ -24,6 +24,7 @@ runesphome ()
 ##### Main
 
 filename=
+override=0
 
 if [ $# -eq 0 ]; then
     echo "Your command line contains no arguments"
@@ -33,14 +34,16 @@ fi
 
 while [ "$1" != "" ]; do
     case $1 in
-        -f | --filename )     shift
-                                filename=$1
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
+        -o | --override )   override=1
+                            ;;
+        -f | --filename )   shift
+                            filename=$1
+                            ;;
+        -h | --help )       usage
+                            exit
+                            ;;
+        * )                 usage
+                            exit 1
     esac
     shift
 done
